db7a1f9735473f946e9109439025ddf028e350ca
olivia <olivia.alexander@uvm.edu>
2025-01-15 18:00:21 -0500

    added more time

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
 for i in range(1000):
-   p.stepSimulation()
-   print(i)
-   time.sleep()
+    p.stepSimulation()
+    print(i)
+    time.sleep(1)
 p.disconnect()
