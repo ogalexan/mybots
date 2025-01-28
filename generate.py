a11b42a5c3a1f81c9cc82de1f83a9cf06dd12b4c
olivia <olivia.alexander@uvm.edu>
2025-01-27 19:56:53 -0500

    x,y,z positions in generate

--- generate.py
+++ generate.py
@@ -1,4 +1,9 @@
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
-pyrosim.End()
+length = 1
+width = 2
+height = 3
+x = 0
+y = 0
+z = 0
+pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height]
--- simulate.py
+++ simulate.py
@@ -3,8 +3,8 @@ import time
 import pybullet_data
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+p.setGravity(0,0,-9.8)
 p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
