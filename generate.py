454c2aaa98d4ef080733780686a8f55c845065ce
olivia <olivia.alexander@uvm.edu>
2025-02-02 16:53:58 -0500

    up to step 20

--- generate.py
+++ generate.py
@@ -1,26 +1,28 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = 0
-# pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-# pyrosim.Send_Cube(name="Box2", pos=[x,y,z], size=[length,width,height])
 
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    length = 1
+    width = 1
+    height = 1
+    x = 0
+    y = 0
+    z = 0
 
-for i in range(-1,2):
-    #pyrosim.Send_Cube(name="Box", pos=[i,y,z], size=[length,width,height])
-    for j in range(-1,2):
-        for k in range(0,3):
-            pyrosim.Send_Cube(name="Box", pos=[i,j,k], size=[0.9 **k,0.9 **k,0.9 **k])
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])      
+    pyrosim.End()
 
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length = 1
+    width = 1
+    height = 1
+    x = 2
+    y = 0
+    z = 0
+   
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size=[length,width,height])
+    pyrosim.End()
 
-#for k in range(1,4):
-    #        pyrosim.Send_Cube(name="Box", pos=[i,j,k], size=[length * (1 - 0.9 **(j-1)),width * (1 - 0.9 **(j-1)),height * (1 - 0.9 **(j-1))])
-
-#boxname = "Box" + str(i+j)
-        
-
-pyrosim.End()
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -4,8 +4,9 @@ import pybullet_data
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 p.setGravity(0,0,-9.8,physicsClient)
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 for i in range(1000):
     p.stepSimulation()
     print(i)
