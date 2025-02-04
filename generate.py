4ed22142e2c9ee282af40afaa27db61e8a6ae2c5
olivia <olivia.alexander@uvm.edu>
2025-02-03 21:54:02 -0500

    joints completed

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ def Create_World():
     y = 0
     z = 0
 
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])      
+    pyrosim.Send_Cube(name="Box", pos=[x,3,z], size=[length,width,height])      
     pyrosim.End()
 
 def Create_Robot():
@@ -17,11 +17,16 @@ def Create_Robot():
     length = 1
     width = 1
     height = 1
-    x = 2
+    x = 0
     y = 0
     z = 0
    
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size=[length,width,height])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_BackLeg",parent="Torso", child="BackLeg", type = "revolute", position = [1.0,0,1.0]) 
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[length,width,height])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg",parent="Torso", child="FrontLeg", type = "revolute", position = [2.0,0,1.0])
+
     pyrosim.End()
 
 Create_World()
